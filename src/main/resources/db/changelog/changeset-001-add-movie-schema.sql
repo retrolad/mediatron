6e2ffaed82cb44ffc6d2703f22a686a6b27adf94
retrolad <retrol4d@gmail.com>
2025-10-30 10:41:17 +0400

    refactor: add constraints to movie ddl

--- src/main/resources/db/changelog/changeset-001-add-movie-schema.sql
+++ src/main/resources/db/changelog/changeset-001-add-movie-schema.sql
@@ -5,17 +5,17 @@ create table if not exists movie
 (
     id bigserial primary key,
     title varchar(255) not null,
-    original_title varchar(255),
-    release_year integer,
+    original_title varchar(255) not null,
+    release_year integer not null,
     description text,
     kinopoisk_id integer,
     imdb_id integer,
     oscar_count smallint,
-    kinopoisk_rating numeric(3,1),
-    imdb_rating numeric(3,1),
-    interest_group_rating numeric(3,1),
-    director varchar(255),
+    kinopoisk_rating float,
+    imdb_rating float,
+    interest_group_rating float,
+    director varchar(255) not null,
     country jsonb,
-    duration smallint
+    duration smallint not null
 );
 
